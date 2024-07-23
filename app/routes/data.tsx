0571eab96ca95c0407f5630c3a563d564b4c4d50
Thanasin169 <thanasin.chu@rmutto.ac.th>
2024-07-23 11:49:04 +0700

    week04
    สร้าง ไฟล์ Data.tsx and myrummitList
    แก้ key

---
+++ app/routes/data.tsx
@@ -0,0 +1,45 @@
+export const sculptureList = [
+ {
+    name:' วงดนตรี ',
+    artist:'คณะขวัญใจ',
+    description:'วงโปรดของฉัน',
+    url:'/images/ab67616d0000b273514184a76a23dff8e3dc0584 (1).jfif',
+    alt:'เป็นเพลงยุคเก่าที่บนปัจุบันชอบฟัง',
+ },
+ {
+    name:' วงดนตรี ',
+    artist:'Silly fools',
+    description:'วงโปรดของฉัน',
+    url:'/images/4eed1b8e770.jpg',
+    alt:'วงเก่าที่ยังมีอยู่ในปัจจุบันแต่เปลี่ยนนักร้องคนใหม่',
+ },
+ {
+    name:' วงดนตรี ',
+    artist:' Paradox ',
+    description:'วงที่หลายๆคนที่รู้จักในวงการเพลง',
+    url:'/images/images (1).jfif',
+    alt:'วงที่สมัยก่อนใครๆก็ต่างรู้จัก',
+ },
+ {
+    name:' การ์ตูน ',
+    artist:' ยอดนักสืบจิ๋ว โคนัน ',
+    description:'ถึงตัวจะเด็กแต่สมองเขาก็ผู้ใหญ่',
+    url:'/images/27cf9de0-1f2a-11ef-9d5c-8f8eda0ae938_webp_original.webp',
+    alt:'การ์ตูนวัยเด็กที่ใครๆก็รู้จักเขาดี',
+ },
+{
+    name:' การ์ตูน ',
+    artist:' ชินจัง ',
+    description:'เด็กที่ทะลิ้งชอบโชว์ตูด',
+    url:'/images/Crayon_Shin-chan.jpg',
+    alt:'การ์ตูนที่อยู่อย่างยาวนานและยังมีอยู่ในปัจจุบัน',
+},
+{
+    name:' การ์ตูน ',
+    artist:' Dr.Slump-Arale Chan ',
+    description:'เด็กที่ชอบจิ้มขี้เป็นชีวิตจิตใจ',
+    url:'/images/2ed122d0-d807-11ea-8433-c5d4d14f3a3c_original.jpg',
+    alt:'ตัวครหลักจะมีอยู่ 2 คน Dr.และหนูน้อย',
+},
+];
+  
